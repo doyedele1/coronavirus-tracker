b8b42b87f25dec5d4b3645a6fc929abbb58d4dc2
doyedele1 <demilade.oyedele@gmail.com>
2021-01-03 00:54:21 +0100

    code refactoring

--- src/api/index.js
+++ src/api/index.js
@@ -11,7 +11,6 @@ export const fetchData = async (country) => {
 
   try {
     const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
-
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {
     return error;
